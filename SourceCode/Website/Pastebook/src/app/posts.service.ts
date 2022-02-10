9da7ac196bcf54173384b1f7f13bf53c8e7fce2d
jplaruscain99 <jp.laruscain.99@gmail.com>
2022-02-10 23:16:33 +0800

    updates

--- SourceCode/Website/Pastebook/src/app/posts.service.ts
+++ SourceCode/Website/Pastebook/src/app/posts.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface PostModel {
+  userId: number;
+  postId: number;
+  datePosted: number;
+  caption: string;
+  photoId: number;
+  photo: string;
+  profileLink: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Object[]> {
+    return this.http.get<any>('http://localhost:5000/postslist');
+  }
+
 }
