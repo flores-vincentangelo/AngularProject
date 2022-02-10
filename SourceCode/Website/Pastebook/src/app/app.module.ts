8d22500a51f4b90454b20b41cd198d68a1a22272
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-10 10:12:44 +0800

    Made profile and header components
    fixed the routes as well

--- SourceCode/Website/Pastebook/src/app/app.module.ts
+++ SourceCode/Website/Pastebook/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ SourceCode/Website/Pastebook/src/app/profile/profile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription | null = null;
+  profileLink: string = '';
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.subscription = this.route.paramMap.subscribe(async (paramMap) => {
+      this.profileLink = paramMap.get('profileLink') as string;
+    });
+  }
+
+  ngOnDestroy(): void {
+      this.subscription?.unsubscribe();
+  }
+
+}
