ec88cef8da7ba57a04042b1092ea3b041e37cad4
jplaruscain99 <jp.laruscain.99@gmail.com>
2022-02-11 16:53:18 +0800

    update

--- SourceCode/Website/Pastebook/src/app/posts/posts.component.ts
+++ SourceCode/Website/Pastebook/src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../home.service';
 import { PostsService } from '../posts.service';
 
 @Component({
@@ -9,15 +10,22 @@ import { PostsService } from '../posts.service';
 export class PostsComponent implements OnInit {
 
   postsList: any[] = [];
+  user: any = null;
   isAddPostVisible = false;
 
-  constructor(private postsService: PostsService) { }
+  constructor(private postsService: PostsService, 
+              private homeService: HomeService) { }
 
   async ngOnInit() {
     this.postsService.getPosts()
       .subscribe(data => {
         this.postsList = data;
       });
+    
+    this.homeService.getUser()
+    .subscribe(data => {
+      this.user = data;
+    });
   }
 
   AddPostVisibleToggle(){
