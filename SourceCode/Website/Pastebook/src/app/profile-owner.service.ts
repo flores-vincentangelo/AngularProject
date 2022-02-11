a56af6c870278c51854fdbd6cbbd9e7296610db9
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-11 15:58:07 +0800

    Update profile-owner.service.ts

--- SourceCode/Website/Pastebook/src/app/profile-owner.service.ts
+++ SourceCode/Website/Pastebook/src/app/profile-owner.service.ts
@@ -2,26 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-
-interface IUser{
-  about: string,
-  birthday: number,
-  cover: string,
-  duplicate: number,
-  emailAddress: string,
-  fullName: string,
-  gender: string,
-  lastName: string,
-  mobileNuber: string,
-  newPassword?: string,
-  password: string,
-  photo: string,
-  profileLink: string,
-  profileName: string,
-  readableBirthday?: string,
-  userId: number
-}
-
 @Injectable({
   providedIn: 'root'
 })
