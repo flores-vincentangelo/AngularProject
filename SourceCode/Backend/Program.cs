1902d45e40a3066b437a639ac7f8bf87b4a7cef5
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-11 17:25:13 +0800

    Update Program.cs

--- SourceCode/Backend/Program.cs
+++ SourceCode/Backend/Program.cs
@@ -6,12 +6,14 @@ class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews()
         .AddJsonOptions(options =>
-        {
-            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-        });
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            });
+        builder.Services.AddCors();
         var app = builder.Build();
         app.UseStaticFiles();
         app.UseRouting();
+        app.UseCors(config => config.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
         app.MapControllers();
         app.UseFileServer();
         app.Run();
